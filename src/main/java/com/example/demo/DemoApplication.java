9945f575ce0f6940fb05d4c73428a52348b95d09
bengke620 <993404028@qq.com>
2019-01-05 02:43:58 +0800

    修改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("dfd");
+		System.out.println("dfd");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
